07381bc80bb50d3971876c1d29d88192e532c035
eemi210 <alainekambi@192.168.1.103>
2015-05-16 19:57:26 +0200

    adds id, and class support to Titanium.UI.Android classes

--- ahome-titanium/src/main/java/com/ait/toolkit/titanium/mobile/client/ui/android/ProgressIndicator.java
+++ ahome-titanium/src/main/java/com/ait/toolkit/titanium/mobile/client/ui/android/ProgressIndicator.java
@@ -1,19 +1,22 @@
 /*
- Copyright (c) 2014 Ahomé Innovation Technologies. All rights reserved.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * Copyright (c) 2014 Ahomé Innovation Technologies. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.ait.toolkit.titanium.mobile.client.ui.android;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import com.ait.toolkit.core.client.JsObject;
 import com.ait.toolkit.titanium.mobile.client.core.events.EventDispatcher;
@@ -40,10 +43,18 @@ import com.google.gwt.core.client.JavaScriptObject;
 public class ProgressIndicator extends EventDispatcher {
 
     public ProgressIndicator() {
-        jsObj = createPeer();
+        jsObj = createPeer( "", new ArrayList<String>() );
     }
 
-    protected ProgressIndicator(JavaScriptObject obj) {
+    public ProgressIndicator( String id, List<String> classes ) {
+        jsObj = createPeer( id, classes );
+    }
+
+    public ProgressIndicator( String id ) {
+        jsObj = createPeer( id, new ArrayList<String>() );
+    }
+
+    protected ProgressIndicator( JavaScriptObject obj ) {
         jsObj = obj;
     }
 
@@ -52,7 +63,7 @@ public class ProgressIndicator extends EventDispatcher {
      * When true allows the user to cancel the progress dialog by pressing the
      * BACK button.
      */
-    public native void setCancelable(boolean value)/*-{
+    public native void setCancelable( boolean value )/*-{
 		var jso = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
 		jso.cancelable = value;
     }-*/;
@@ -62,7 +73,7 @@ public class ProgressIndicator extends EventDispatcher {
 		return jso.cancelable;
     }-*/;
 
-    public native void setMax(double value)/*-{
+    public native void setMax( double value )/*-{
 		var jso = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
 		jso.max = value;
     }-*/;
@@ -72,7 +83,7 @@ public class ProgressIndicator extends EventDispatcher {
 		return jso.max;
     }-*/;
 
-    public native void setMessage(String value)/*-{
+    public native void setMessage( String value )/*-{
 		var jso = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
 		jso.message = value;
     }-*/;
@@ -82,7 +93,7 @@ public class ProgressIndicator extends EventDispatcher {
 		return jso.message;
     }-*/;
 
-    public native void setMessageId(String value)/*-{
+    public native void setMessageId( String value )/*-{
 		var jso = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
 		jso.messageid = value;
     }-*/;
@@ -92,7 +103,7 @@ public class ProgressIndicator extends EventDispatcher {
 		return jso.messageid;
     }-*/;
 
-    public native void setMin(double value)/*-{
+    public native void setMin( double value )/*-{
 		var jso = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
 		jso.min = value;
     }-*/;
@@ -102,7 +113,7 @@ public class ProgressIndicator extends EventDispatcher {
 		return jso.min;
     }-*/;
 
-    public native void setType(double value)/*-{
+    public native void setType( double value )/*-{
 		var jso = this.@com.ait.toolkit.core.client.JsObject::getJsObj()();
 		jso.type = value;
     }-*/;
@@ -128,16 +139,20 @@ public class ProgressIndicator extends EventDispatcher {
      * The user triggers this event by pressing the BACK button when the dialog
      * is visible. The dialog will be hidden and this event dispatched.
      */
-    public void addCancelHandler(EventHandler handler) {
-        this.addEventHandler("cancel", handler);
+    public void addCancelHandler( EventHandler handler ) {
+        this.addEventHandler( "cancel", handler );
     }
 
-    public static ProgressIndicator cast(JsObject peer) {
-        return new ProgressIndicator(peer.getJsObj());
+    public static ProgressIndicator cast( JsObject peer ) {
+        return new ProgressIndicator( peer.getJsObj() );
     }
 
-    private native JavaScriptObject createPeer()/*-{
-		return Ti.UI.Android.createProgressIndicator();
+    private native JavaScriptObject createPeer( String elId, List<String> classes )/*-{
+		var cls = @com.ait.toolkit.titanium.mobile.client.ui.UI::_createClassList(Ljava/util/List;)(classes);
+		return Ti.UI.Android.createProgressIndicator({
+			id : elId,
+			classes : cls
+		});
     }-*/;
 
 }
